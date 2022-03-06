c7c86b08f369a3c9298c50aa2ad7bf8cd3b80217
johntarus <user email>
2022-03-06 17:21:39 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+const Footer = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        bottom: "0",
+        paddingTop: "10px",
+        paddingBottom: "10px",
+        width: "100%",
+        textAlign: "center",
+        backgroundColor: "white",
+      }}
+    >
+      <Typography></Typography>
+    </div>
+  );
+};
+
+export default Footer;
